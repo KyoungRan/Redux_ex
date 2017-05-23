778c8aca9000d78f730eb41393b9915dd5fbde2c
KyoungRan <kyoungrankang@gmail.com>
2017-05-24 01:16:43 +0900

    completed making counter

--- redux-counter/src/actions/index.js
+++ redux-counter/src/actions/index.js
@@ -1,7 +1,7 @@
 /*
-* action 객체를 만드는 액션 생성자들을 선언합니다. (action creators)
-* 여기서 () => ({}) 은, function() { return { }} 와 동일한 의미입니다. 
-* scope 이슈와 관계 없이 편의상 사용되었습니다. 
+    action 객체를 만드는 액션 생성자들을 선언합니다. (action creators)
+    여기서 () => ({}) 은, function() { return { } } 와 동일한 의미입니다.
+    scope 이슈와 관계 없이 편의상 사용되었습니다.
 */
 import * as types from './ActionTypes';
 
@@ -13,7 +13,7 @@ export const decrement = () => ({
     type: types.DECREMENT
 });
 
-// 다른 액션 생성자들과 달리, 파라미터를 갖고 있습니다.
+// 다른 액션 생성자들과 달리, 파라미터를 갖고있습니다
 export const setColor = color => ({
     type: types.SET_COLOR,
     color
--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -4,12 +4,12 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-// Redux 관련 불러오괴
+// Redux 관련 불러오기
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Provider } from 'react-redux';
 
-// store 생성
+// 스토어 생성
 const store = createStore(reducers);
 
 ReactDOM.render(
