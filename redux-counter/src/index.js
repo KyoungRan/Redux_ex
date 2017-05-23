54abd599b67c7698201d9989a5d160b6bf497f42
KyoungRan <kyoungrankang@gmail.com>
2017-05-23 23:47:29 +0900

    added actions, reducers and modified App, index.js

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -4,5 +4,18 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux 관련 불러오괴
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+// store 생성
+const store = createStore(reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
