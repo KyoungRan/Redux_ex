18242a2159001954a72ad885284b1b0dbf6be6fa
KyoungRan <kyoungrankang@gmail.com>
2017-05-24 12:48:01 +0900

    modified and added Ducks structure and redux-actions

---
+++ redux-counter/src/README.md
@@ -0,0 +1,3 @@
+이 프로젝트는 아래 예제를 복습한 것입니다. 
+Redux 정복하기 from VELOPERT
+https://velopert.com/
--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 // Redux 관련 불러오기
 import { createStore } from 'redux';
-import reducers from './reducers';
+import reducers from './modules';
 import { Provider } from 'react-redux';
 
 // 스토어 생성
